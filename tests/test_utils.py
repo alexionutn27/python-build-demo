9d0134b28ede380dc0a4b34784e603dd39cd9d83
alexionutn27 <alexionutn27@gmail.com>
2026-02-16 23:37:54 +0200

    test failed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,3 +3,5 @@ from utils import calculate_total
 def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
     assert calculate_total([]) == 0
+def test_intentional_fail():
+    assert 2 + 2 == 5
