d44290875b89f81bd3dffc3dcb04b10d78d552a6
alexionutn27 <alexionutn27@gmail.com>
2026-02-17 00:15:07 +0200

    flake8 failed update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,5 +4,6 @@ from utils import calculate_total
 def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
 
-    
-assert calculate_total([]) == 0
+
+def test_empty_list():
+    assert calculate_total([]) == 0
